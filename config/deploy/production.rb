128836fbb7fbf6b89d84c78ab600123c6384a276
ec0341ks <with0526@icloud.com>
2019-06-27 21:20:23 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '13.113.2.237', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
