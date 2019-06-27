e0ce48e5605aded7d5d88dad67a469792fec8d0e
ec0341ks <with0526@icloud.com>
2019-06-28 00:00:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.11.0"
 
 set :application, "freemarket_sample_49a"
-set :repo_url, "git@example.com:pigutty/freemarket_sample_49a.git"
+set :repo_url, "git@github.com:pigutty/freemarket_sample_49a.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
