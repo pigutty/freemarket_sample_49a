1d8e8971ec803917ed8ea945a7fa2597f3131693
ec0341ks <with0526@icloud.com>
2019-07-05 19:31:44 +0900

    carriewave test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,39 @@
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
+
+# CarrierWave.configure do |config|
+#   if Rails.env.development? || Rails.env.test?
+#     config.storage = :file
+
+#   elsif Rails.env.production?
+#     config.storage = :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+#     region: 'ap-northeast-1'
+#   }
+#     config.fog_directory  = 'freemarket-sample-49a'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-49a'
+#   end
+# end
+
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-
-  elsif Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: 'test',
+    aws_secret_access_key: 'test',
     region: 'ap-northeast-1'
   }
-    config.fog_directory  = 'freemarket-sample-49a'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-49a'
-  end
+
+  config.fog_directory  = 'freemarket-sample-49a'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-49a'
 end
