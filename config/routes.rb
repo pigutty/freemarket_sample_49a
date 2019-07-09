b4730fafa1dd95c0cf8397141a1af8494fd70336
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-09 21:49:03 +0900

    不要なファイルの消去とアクションの明記

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    resources :products
-      resources :category
+  resources :users, only: [:index]
+  resources :products, only:[:show, :new] do
+    resources :category
   end
 end
