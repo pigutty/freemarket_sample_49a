d66edbd601093c4785c4142eeae30a1ffddf86e5
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-09 19:41:43 +0900

    カテゴリテーブルの作成
    big_id, middle_idでツリー構造を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'top#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products
+    resources :category
 end
