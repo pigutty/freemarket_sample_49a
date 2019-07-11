19848c7f4d800936915c874452c08406a78a8ac0
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-11 21:33:37 +0900

    静的なビュー完成

---
+++ app/assets/javascripts/mypage_main.js
@@ -0,0 +1,7 @@
+// $(function(){
+//   $(".mypage__main__information__tab__title").on('click',function(){
+//     // $(".mypage__main__information__tab__title").find('a').removeClass("active");
+//     $(".mypage__main__information__tab__title").addClass("inactive");
+//     // $(this).children('a').addClass('active');
+//   });
+// });
