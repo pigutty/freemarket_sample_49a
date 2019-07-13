b170f15e5da55ad431afacd6596137edd4b37aa1
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-13 15:09:33 +0900

    本人確認のためのcontrollerとビュー設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'top#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index]
+  resources :user_identifications, only: [:index]
   resources :products, only:[:show, :new] do
     resources :category
   end
