e256c98afe1a906adff5e0dab26cb140bb3fec91
xkatsuyax <ohagi.p@gmail.com>
2019-07-19 21:13:04 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'signup#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'top#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index]
   resources :products, only:[:show, :new] do
