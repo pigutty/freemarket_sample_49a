656d6eab37ddcfde9ab18692ee508e98d28396f4
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-25 19:08:55 +0900

    コンフリクト解消

--- app/controllers/user_identifications_controller.rb
+++ app/controllers/user_identifications_controller.rb
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-class UserIdentificationsController < TopController
-=======
-<<<<<<< Updated upstream
-class UserIdentificationsController < ApplicationController
-=======
 class UserIdentificationsController < UsersController
->>>>>>> Stashed changes
->>>>>>> ce5f872... 本人確認のビュー調整
   def index
   end
 end
