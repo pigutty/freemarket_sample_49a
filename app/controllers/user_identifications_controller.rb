ce5f87292113019e761e5fed198bc05ec8f4f8aa
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-25 18:57:42 +0900

    本人確認のビュー調整

--- app/controllers/user_identifications_controller.rb
+++ app/controllers/user_identifications_controller.rb
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 class UserIdentificationsController < ApplicationController
+=======
+class UserIdentificationsController < UsersController
+>>>>>>> Stashed changes
   def index
   end
 end
