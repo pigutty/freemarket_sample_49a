0b4dac12d6dd73ba13034d0660b14a138db3bad2
ec0341ks <with0526@icloud.com>
2019-07-27 14:38:30 +0900

    form_withの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,16 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def create
+    @product = Product.new(listing_params)
+  end
+
   def buy
+  end
+
+  private
+
+  def listing_params
+    params.require(:product).permit(:name)
   end
 end
