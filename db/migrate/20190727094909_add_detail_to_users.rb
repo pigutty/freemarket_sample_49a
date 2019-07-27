5f9977e4e452b8f4e2ae7ee3907a5fd20106362f
xkatsuyax <ohagi.p@gmail.com>
2019-07-27 18:51:07 +0900

    ユーザーテーブルのカラム名追加

---
+++ db/migrate/20190727094909_add_detail_to_users.rb
@@ -0,0 +1,11 @@
+class AddDetailToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :passward, :string, null: false
+    add_column :users, :family_name, :string, null: false
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :family_furigana, :string, null: false
+    add_column :users, :first_furigana, :string, null: false
+    add_column :users, :birth_day, :date, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190708151659) do
+ActiveRecord::Schema.define(version: 20190727094909) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.integer  "big_id"
+    t.integer  "middle_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -20,6 +28,13 @@ ActiveRecord::Schema.define(version: 20190708151659) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "nickname",                            null: false
+    t.string   "passward",                            null: false
+    t.string   "family_name",                         null: false
+    t.string   "first_name",                          null: false
+    t.string   "family_furigana",                     null: false
+    t.string   "first_furigana",                      null: false
+    t.date     "birth_day",                           null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
