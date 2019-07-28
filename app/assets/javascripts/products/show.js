f1ae574feafeca79e642afd6b6c691bb2dc73a20
Hiroyuki-Nakamura <yhtte8kbrx@gmail.com>
2019-07-28 11:32:34 +0900

    スライダー追加

--- app/assets/javascripts/products/show.js
+++ app/assets/javascripts/products/show.js
@@ -1,27 +1,28 @@
-$(function () {
-  var slider = '.item-detail__image__sliders';
-  var thumbnailItem = '.item-detail__image__thumbnail';
-  //サムネイルのインデックス番号を取得
-  $(thumbnailItem).each(function () {
-    var index = $(thumbnailItem).index(this);
+$(document).on('turbolinks:load', function(){
+  var slider = ".item-detail__image__sliders";
+  var thumbnailItem = ".item-detail__image__thumbnail";
+
+  $(thumbnailItem).each(function(){
+   var index = $(thumbnailItem).index(this);
+   $(this).attr("data-index",index);
   });
-  $(slider).on('init', function () {
-    var index = $('.slick-slide.slick-current').attr('data-slick-index');
-    $(thumbnailItem + '[data-index="'+index+'"]').addClass('thumbnail-current');
+
+  $(slider).on('init',function(slick){
+   var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
+   $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
   });
 
   $(slider).slick({
     arrows: false,
+    infinite: false
   });
-
   $(thumbnailItem).on('click',function(){
-    var index = $(this).attr('data-index');
+    var index = $(this).attr("data-index");
     $(slider).slick("slickGoTo",index,false);
   });
-
-  $(slider).on('beforeChange',function(event,slick,currentSlide,nextSlide){
+  $(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
     $(thumbnailItem).each(function(){
-      $(this).removeClass('thumbnail-current');
+      $(this).removeClass("thumbnail-current");
     });
     $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
   });
