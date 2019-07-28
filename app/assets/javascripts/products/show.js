400bf41442719eea820260894d28149accec3d13
Hiroyuki-Nakamura <yhtte8kbrx@gmail.com>
2019-07-28 12:32:03 +0900

    fix
    # WHAT
    スライダー部分の不要な記述を削除
    # WHY
    コードの可読性を上げるため

--- app/assets/javascripts/products/show.js
+++ app/assets/javascripts/products/show.js
@@ -7,23 +7,13 @@ $(document).on('turbolinks:load', function(){
    $(this).attr("data-index",index);
   });
 
-  $(slider).on('init',function(slick){
-   var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
-   $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
-  });
-
   $(slider).slick({
     arrows: false,
     infinite: false
   });
+
   $(thumbnailItem).on('click',function(){
     var index = $(this).attr("data-index");
     $(slider).slick("slickGoTo",index,false);
-  });
-  $(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
-    $(thumbnailItem).each(function(){
-      $(this).removeClass("thumbnail-current");
-    });
-    $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
   });
 });
