30a314470608826e8642f57c651b79204e50771c
pigutty <50047826+pigutty@users.noreply.github.com>
2019-07-28 16:18:23 +0900

    ユーザー新規登録の際にcredit_cardのcustomer_idを登録するようにした

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,14 +1,19 @@
 class CreditCardsController < ApplicationController
   require 'payjp'
-  Payjp.api_key = ENV["SECRET_PAYJP_API_KEY"]
+  Payjp.api_key = "sk_test_16aaae6844b13bff01d1bd1e"
+  # Payjp.api_key = ENV["SECRET_PAYJP_API_KEY"]
   # 環境変数/etc/envに設定してもらう。
   def new
   end
 
   def create
     # @user = User.find(params[id:current_user.id]) ユーザー登録後に追記する
+    @user = User.find(1)
     create_token(credit_card_params)
+    @user.customer_id = @customer.id
+    @user.save
     CreditCard.create(token_id: @token_id, user_id: 1)
+    binding.pry
   end
 
   private
@@ -28,8 +33,9 @@ class CreditCardsController < ApplicationController
       'X-Payjp-Direct-Token-Generate': 'true'
     })
     @token_id = @token.id
+    @customer = Payjp::Customer.create()
     # @customer = Payjp::Customer.retrieve(@user.customer_id)
-    # @customer.cards.create(card:@token_id)
+    @customer.cards.create(card:@token_id)
   end
 
 
