3204d62a7ed9d3b1c65c32a1819055a64e7d0d0e
xkatsuyax <ohagi.p@gmail.com>
2019-07-29 09:53:31 +0900

    新規登録データベース変更とフォーム作成

--- db/migrate/20190727094909_add_detail_to_users.rb
+++ db/migrate/20190727094909_add_detail_to_users.rb
@@ -1,7 +1,6 @@
 class AddDetailToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :nickname, :string, null: false
-    add_column :users, :passward, :string, null: false
     add_column :users, :family_name, :string, null: false
     add_column :users, :first_name, :string, null: false
     add_column :users, :family_furigana, :string, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190727094909) do
+ActiveRecord::Schema.define(version: 2019_07_27_094909) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.integer  "big_id"
-    t.integer  "middle_id"
+  create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "big_id"
+    t.integer "middle_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "nickname",                            null: false
-    t.string   "passward",                            null: false
-    t.string   "family_name",                         null: false
-    t.string   "first_name",                          null: false
-    t.string   "family_furigana",                     null: false
-    t.string   "first_furigana",                      null: false
-    t.date     "birth_day",                           null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_furigana", null: false
+    t.string "first_furigana", null: false
+    t.date "birth_day", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
