c12ae489facf60a1bf597022e7f7b05333f2c930
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-02 19:55:49 +0900

    ビューをform_withで書き直し

--- app/controllers/address_registration_controller.rb
+++ app/controllers/address_registration_controller.rb
@@ -1,4 +1,7 @@
 class AddressRegistrationController < ApplicationController
   def new
   end
+
+  def create
+  end
 end
--- app/controllers/phone_number_authorization_controller.rb
+++ app/controllers/phone_number_authorization_controller.rb
@@ -1,4 +1,7 @@
 class PhoneNumberAuthorizationController < ApplicationController
   def new
   end
+
+  def create
+  end
 end
