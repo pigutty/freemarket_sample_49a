6c9606601cebaf835569bdad176d420b0754d664
ec0341ks <with0526@icloud.com>
2019-08-03 15:32:36 +0900

    Update top_controller.rb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,10 @@
 class TopController < ApplicationController
   before_action :big_categories
   def index
-    category = Categoryactive.new
-    @bigcategories = Categoryactive.where(parent_id:nil, child_id:nil)
+    category = Category.new
+    @bigcategories = Category.where(parent_id:nil, child_id:nil)
     parent_id = params[:parent_id].to_i
-    @middlecategories = Categoryactive.where(parent_id: parent_id).take(14)
+    @middlecategories = Category.where(parent_id: parent_id).take(14)
     respond_to do |format|
       format.html
       format.json
@@ -12,6 +12,6 @@ class TopController < ApplicationController
   end
 
   def big_categories
-    @bigcategories = Categoryactive.where(parent_id:nil, child_id:nil)
+    @bigcategories = Category.where(parent_id:nil, child_id:nil)
   end
 end
