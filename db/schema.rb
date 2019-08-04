79e81556f75637d34e23fa1d66b9616c69fa5cf5
xkatsuyax <ohagi.p@gmail.com>
2019-08-04 13:48:43 +0900

    マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_094909) do
+ActiveRecord::Schema.define(version: 2019_07_28_081813) do
 
-  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "token_id", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
+  end
+
+  create_table "products", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "size_id", null: false
+    t.string "brand"
+    t.integer "status_id", null: false
+    t.boolean "shipping_fee_id", null: false
+    t.integer "shipping_date_id", null: false
+    t.integer "price", null: false
+    t.integer "category_id"
+    t.integer "user_id"
+    t.integer "prefecture_id", null: false
+  end
+
+  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "purchase_id"
+    t.integer "evaluation"
+    t.integer "product_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "fk_rails_0d630f0bc7"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "customer_id"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -32,4 +66,7 @@ ActiveRecord::Schema.define(version: 2019_07_27_094909) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "credit_cards", "users"
+  add_foreign_key "purchases", "products"
+  add_foreign_key "purchases", "users"
 end
