cee2fa5ce43ab3d55dda62794144815ed7ead39f
tantan1984 <tantan198409@gmail.com>
2019-08-04 16:07:36 +0900

    DBから情報を引っ張り出す
    商品一覧にDBから情報を引っ張ってこれるようにしました。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,17 +1,18 @@
 class TopController < ApplicationController
   before_action :big_categories
   def index
-    category = Categoryactive.new
-    @bigcategories = Categoryactive.where(parent_id:nil, child_id:nil)
+    category = Category.new
+    @bigcategores = Category.where(parent_id:nil, child_id:nil)
     parent_id = params[:parent_id].to_i
-    @middlecategories = Categoryactive.where(parent_id: parent_id).take(14)
+    @middlecategories = Category.where(parent_id: parent_id).take(14)
     respond_to do |format|
       format.html
       format.json
+    @products = Product.all.order("id ASC")
     end
   end
 
   def big_categories
-    @bigcategories = Categoryactive.where(parent_id:nil, child_id:nil)
+    @bigcategories = Category.where(parent_id:nil, child_id:nil)
   end
 end
