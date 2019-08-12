28dde9d32fdbb4261bfd36ccdf200b35d22c4d89
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-12 11:59:09 +0900

    ユーザーサイン機能ができたので、current_userに書き直し！

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,7 @@
 class PurchasesController < ApplicationController
   def new
     @product = Product.find(params[:product_id])
-    @user = User.find(1)
-    # ユーザー登録終了後は@userはcurrent_userに直す
-    @customer = Payjp::Customer.retrieve(@user.customer_id)
+    @customer = Payjp::Customer.retrieve(current_user.customer_id)
     card = @customer.cards.data[0]
     # カードが複数ある場合にどのカードを選ぶのかは問題だが、とりあえず一番目
     @brand = card.brand
@@ -14,19 +12,18 @@ class PurchasesController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-    @user = User.find(1)
     charge = Payjp::Charge.create(
       amount: @product.price,
-      customer: @user.customer_id,
+      customer: current_user.customer_id,
       currency: 'jpy',
     )
-    Purchase.create(purchase_id:charge.id,product_id: params[:product_id],user_id:@user.id)
+    Purchase.create(purchase_id:charge.id,product_id: params[:product_id],user_id:current_user.id)
     redirect_to root_path
   end
 
   private
 
   def purchase_params
-    params.permit(:product_id).merge(@user.id,@user.customer_id)
+    params.permit(:product_id).merge(current_user.id,current_user.customer_id)
   end
 end
