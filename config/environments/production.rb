b38decc3f03eeb9d602e1856a63d27c1bdf54422
ec0341ks <with0526@icloud.com>
2019-08-13 13:40:11 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # Store files on Amazon S3.
-  application.config.active_storage.service = :amazon
+  config.active_storage.service = :amazon
 end
