5cd7d8ea5e0ad28522909a7c1a59e272717db2e4
ec0341ks <with0526@icloud.com>
2019-08-13 17:41:16 +0900

    jsの追加

---
+++ app/assets/javascripts/products/category.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function(){
+  $('.listing__form__detail__box__form-group__category-select').eq(0).change(function(){
+    $('.listing__form__detail__box__form-group').eq(1).css('display','block');
+  });
+
+  $('.listing__form__detail__box__form-group__category-select').eq(1).change(function(){
+    console.log('test');
+    $('.listing__form__detail__box__form-group').eq(2).css('display','block');
+  });
+});
