cdbedb15bc358bf4e1d19b4be8213c1ef9abe1b8
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-13 16:56:09 +0900

    登録完了画面のステップバー

--- app/controllers/registration_check_controller.rb
+++ app/controllers/registration_check_controller.rb
@@ -1,4 +1,11 @@
 class RegistrationCheckController < ApplicationController
   def index
+    @member_information_title = nil
+    @member_information = "completed"
+    @phone_information = 'completed'
+    @address_information = 'completed'
+    @credit_information = 'completed'
+    @registration_check_title = 'visited'
+    @registration_check = 'progressed'
   end
 end
