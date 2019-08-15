9447f3a29dce2db6c72726d6c4d112908cf9df48
tantan1984 <tantan198409@gmail.com>
2019-08-15 10:02:18 +0900

    商品一覧ページの見た目を整える２
    画像の表示のされかたや配置の感覚を整えました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_081813) do
+ActiveRecord::Schema.define(version: 2019_08_04_110915) do
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "token_id", null: false
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_07_28_081813) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
+  end
+
+  create_table "product_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.string "image_url"
   end
 
   create_table "products", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
