2eb965f4c03a801cd222f07e335c2c0c293e1a6a
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-15 17:25:50 +0900

    ユーザーデータに欠けがある場合、ユーザー毎消去する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   protect_from_forgery with: :exception
-  # before_action :registration_check unless :devise_controller? || controller_name == 'phone_number_authorizations' || 'address_registration'|| 'credit_cards' 
+  before_action :registration_check  
   before_action :registration_view_params
   require 'payjp'
   Payjp.api_key=Rails.application.credentials.payjp_secret_key
@@ -23,15 +23,20 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :family_name, :first_name, :family_furigana, :first_furigana, :birth_year, :birth_month, :birth_day])
   end
 
-  def after_sign_up_path_for(resource)
-    redirect_to controller:'/phone_number_authorization', action:'new'
-  end
 
-  # def registration_check
-  #   if current_user != nil && current_user.phone_number_authorization != nil && current_user.address_registration != nil && current_user.credit_card != nil
-  #     redirect_to controller: 'devise', action:'destroy'
-  #   end
-  # end
+  def registration_check
+    # ユーザーがログインしている　かつ　登録の画面でない場合　→　中途半端な登録データを削除しroot_pathに移動
+    if user_signed_in?
+      unless ((controller_name == "phone_number_authorization" && action_name == "new")||(controller_name == "address_registration" && action_name == "new")||(controller_name == "credit_cards" && action_name == "new"))
+        if (current_user.phone_number_authorizations.length == 0 || current_user.address_registrations.length == 0 || current_user.credit_cards.length == 0)
+          unless (current_user.phone_number_authorizations.length != 0 && current_user.address_registrations.lenght != 0 && current_user.credit_cards.length != 0)
+            current_user.destroy
+            redirect_to root_path
+          end
+        end
+      end
+    end
+  end
 
   def registration_view_params
     @member_information_title = "visited"
