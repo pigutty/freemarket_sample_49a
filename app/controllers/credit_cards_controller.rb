44488b1e0bcc5744e5a230005323cfec6a75451d
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-16 07:43:12 +0900

    binding.pry消去

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,6 +1,5 @@
 class CreditCardsController < ApplicationController
   def new
-    binding.pry
     if (user_signed_in? && current_user.phone_number_authorizations.length != 0 && current_user.address_registrations.length !=0 && current_user.credit_cards.length ==0)    
       @member_information_title = nil
       @member_information = "completed"
--- app/controllers/phone_number_authorization_controller.rb
+++ app/controllers/phone_number_authorization_controller.rb
@@ -1,6 +1,5 @@
 class PhoneNumberAuthorizationController < ApplicationController
   def new
-    binding.pry
     if (user_signed_in? && current_user.phone_number_authorizations.length == 0 && current_user.address_registrations.length ==0 && current_user.credit_cards.length ==0)
       @member_information_title = nil
       @member_information = "completed"
@@ -13,7 +12,6 @@ class PhoneNumberAuthorizationController < ApplicationController
   end
 
   def create
-    binding.pry
     PhoneNumberAuthorization.create(phone_params)
     redirect_to controller:"address_registration", action:'new'
   end
