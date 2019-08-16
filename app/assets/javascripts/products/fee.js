fa3df6b5e49c646e4c764a7e2a94b0cd6e65f4b3
ec0341ks <with0526@icloud.com>
2019-08-17 00:41:56 +0900

    Update fee.js

--- app/assets/javascripts/products/fee.js
+++ app/assets/javascripts/products/fee.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load',function(){
   $('.listing__form__price__box__price__right__default').on('keyup',function(){
     var price = $('.listing__form__price__box__price__right__default').val();
     if (price >=300 && price <= 9999999){
-      var salesfee = price * 0.1
+      var salesfee = Math.round(price * 0.1)
       var salesprofit = price - salesfee
       $('#salesfee').text('¥' + String(salesfee).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
       $('#salesprofit').text('¥' + String(salesprofit).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
