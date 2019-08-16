7ee31bfee7c53cf9bec122ff8d2c5b037bcb8b1c
ec0341ks <with0526@icloud.com>
2019-08-16 22:06:37 +0900

    販売価格表示の実装

--- app/assets/javascripts/products/fee.js
+++ app/assets/javascripts/products/fee.js
@@ -1,7 +1,9 @@
-$(document).on('turbolinks:load', function(){
-  $('.listing__form__price__box__price__right__default').change(function(){
-    console.log('ここ');
-    var price = ('.listing__form__price__box__price__right__default').val();
-    console.log(price);
-  });
+$(document).on('turbolinks:load',function(){
+  $('.listing__form__price__box__price__right__default').on('keyup',function(){
+    var price = $('.listing__form__price__box__price__right__default').val();
+    var salesfee = price * 0.1
+    var salesprofit = price - salesfee
+    $('#salesfee').text(salesfee);
+    $('#salesprofit').text(salesprofit);
+  })
 });
