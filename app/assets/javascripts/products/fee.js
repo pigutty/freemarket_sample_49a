5c27b11b82446d087ab2d83dc06f1d19e43ced3c
ec0341ks <with0526@icloud.com>
2019-08-16 23:27:12 +0900

    コードの追加

--- app/assets/javascripts/products/fee.js
+++ app/assets/javascripts/products/fee.js
@@ -1,9 +1,14 @@
 $(document).on('turbolinks:load',function(){
   $('.listing__form__price__box__price__right__default').on('keyup',function(){
     var price = $('.listing__form__price__box__price__right__default').val();
-    var salesfee = price * 0.1
-    var salesprofit = price - salesfee
-    $('#salesfee').text(salesfee);
-    $('#salesprofit').text(salesprofit);
+    if (price >=300 && price <= 9999999){
+      var salesfee = price * 0.1
+      var salesprofit = price - salesfee
+      $('#salesfee').text('¥' + String(salesfee).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+      $('#salesprofit').text('¥' + String(salesprofit).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+    }
+    else{
+      return;
+    }
   })
 });
