866bab8c4311a0987b84d002db5b43a6aea0d99c
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-16 23:39:12 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
 User.create(email:'test2@gmail.com',password:'password',nickname:'yodo',family_name:'yodogawa',first_name:'genta',family_furigana:'ヨドガワ',first_furigana:'ゲンタ',birth_year:1993,birth_month:1,birth_day:1)
 
-Product.create(name:'test',description:'test-description',size_id:1,brand:'brand',status_id:1,shipping_fee_id:true,shipping_date_id:1,price:1000,category_id:1,user_id:1,prefecture_id:1)
->>>>>>> 4428b7ac381bf9b53db9dbe2733a40305c234517
+Product.create(name:'test',description:'test-description',size_id:1,brand:'brand',status_id:1,shipping_fee_id:true,shipping_date_id:1,shipping_method_id:1,purchase_status_id:1,price:1000,category_id:1000,user_id:1,prefecture_id:1)
