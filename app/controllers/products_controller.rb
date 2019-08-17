390cafb4a0bfcd3438de27d7ebe104d4ea8dc902
ec0341ks <with0526@icloud.com>
2019-08-17 21:58:33 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
 
   private
   def listing_params
-    params.require(:product).permit(:name, :description, :category_id, :size_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, images: [])
+    params.require(:product).permit(:name, :description, :category_id, :size_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, images: []).merge(purchase_status_id:1)
   end
 
 end
