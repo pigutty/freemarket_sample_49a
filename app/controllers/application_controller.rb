74805375388ebc258592ce4c9c6c814d208affa1
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-17 21:52:27 +0900

    ユーザー登録のためのコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,27 +24,27 @@ class ApplicationController < ActionController::Base
   end
 
   # ユーザー登録画面であればtrue, そうでなければfalse
-  def registration_view_check
-    (controller_name == "phone_number_authorization" && (action_name == "new"||'create'))||(controller_name == "address_registration" && (action_name == "new"||'create'))||(controller_name == "credit_cards" && (action_name == "new"||'create'))
-  end
+  # def registration_view_check
+  #   (controller_name == "phone_number_authorization" && (action_name == "new"||'create'))||(controller_name == "address_registration" && (action_name == "new"||'create'))||(controller_name == "credit_cards" && (action_name == "new"||'create'))
+  # end
 
   # ユーザーのデータに空白があるかどうかをチェック
-  def registration_data_check
-    current_user.phone_number_authorizations.length == 0 || current_user.address_registrations.length == 0 || current_user.credit_cards.length == 0
-  end
+  # def registration_data_check
+  #   current_user.phone_number_authorizations.length == 0 || current_user.address_registrations.length == 0 || current_user.credit_cards.length == 0
+  # end
 
 
-  def registration_check
-    # ユーザーがログインしている　かつ　登録の途中でない場合　かつ　データに欠損がある→　中途半端な登録データを削除しroot_pathに移動
-    if user_signed_in?
-      unless (registration_view_check)
-        if (registration_data_check)
-            current_user.destroy
-            redirect_to root_path
-        end
-      end
-    end
-  end
+  # def registration_check
+  #   # ユーザーがログインしている　かつ　登録の途中でない場合　かつ　データに欠損がある→　中途半端な登録データを削除しroot_pathに移動
+  #   if user_signed_in?
+  #     unless (registration_view_check)
+  #       if (registration_data_check)
+  #           current_user.destroy
+  #           redirect_to root_path
+  #       end
+  #     end
+  #   end
+  # end
 
   def registration_view_params
     @member_information_title = "visited"
