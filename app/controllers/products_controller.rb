a76d927c857b13651b8e9b1ebe80def689feda0c
Hiroyuki-Nakamura <yhtte8kbrx@gmail.com>
2019-08-18 17:35:50 +0900

    fix
    binding.pryの記述を消去

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,6 @@ class ProductsController < TopController
     @grand_child_category = Category.find(@child_category.parent_id)
     @user_products = Product.where(user_id:@product.user_id).where.not(id:@product.id).limit(6).order('id DESC')
     @related_products = Product.where(category_id:@product.category_id).where.not(id:@product.id).limit(6).order('id DESC')
-    # binding.pry
   end
 
   def new
@@ -14,20 +13,23 @@ class ProductsController < TopController
   end
 
   def create
-    binding.pry
     @product = Product.create(listing_params)
-    binding.pry
   end
 
   def buy
   end
 
+  def edit_product
+    @product = Product.find(params[:id])
+  end
+
   def destroy
-    product = Product.find(params[:id])
-      if product.user_id == current_user.id
-        product.delete
+    @product = Product.find(params[:id])
+      if @product.user_id == current_user.id
+        @product.delete
       end
   end
+
 
   private
   def listing_params
