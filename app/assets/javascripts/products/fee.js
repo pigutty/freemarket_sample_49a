ba1c17e7eb829131bc270472ce478fa1d983a963
ec0341ks <with0526@icloud.com>
2019-08-24 12:15:42 +0900

    Update fee.js

--- app/assets/javascripts/products/fee.js
+++ app/assets/javascripts/products/fee.js
@@ -1,4 +1,14 @@
 $(document).on('turbolinks:load',function(){
+  var price = $('.listing__form__price__box__price__right__default').val();
+  console.log(price);
+  if(price != 0){
+    console.log('ここ');
+    var salesfee = Math.round(price * 0.1)
+    var salesprofit = price - salesfee
+    $('#salesfee').text('¥' + String(salesfee).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+    $('#salesprofit').text('¥' + String(salesprofit).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  }
+
   $('.listing__form__price__box__price__right__default').on('keyup',function(){
     var price = $('.listing__form__price__box__price__right__default').val();
     if (price >=300 && price <= 9999999){
