6cc3ca05d4a94bf3aebb5c825384a33ac2008aad
tantan1984 <tantan198409@gmail.com>
2019-08-24 20:33:41 +0900

    プルリクの変更
    プルリクで指摘をされた変更諸々

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,6 @@ class TopController < ApplicationController
   before_action :big_categories
   def index
     category = Category.new
-    @bigcategores = Category.where(parent_id:nil, child_id:nil)
     parent_id = params[:parent_id].to_i
     @middlecategories = Category.where(parent_id: parent_id).take(14)
     respond_to do |format|
