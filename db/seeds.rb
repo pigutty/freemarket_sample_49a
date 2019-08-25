0707ddf495be3ed4ec818d43517ee1221c2fe7f8
tantan1984 <tantan198409@gmail.com>
2019-08-25 20:50:47 +0900

    変数の変更
    メンター様にご指摘いただいた変数を変更しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ test_user = User.create(email:'test2@gmail.com',password:'password',nickname:'yo
 
 num = 0
 while num < 1000 do
-  i = [1,2,3,4,5,6,7,8,9,10,11,12,13].sample
-  j = Category.find(i).children.sample.id
-  k = Category.find(j).grandchildren.sample.id  
+  category = [1,2,3,4,5,6,7,8,9,10,11,12,13].sample
+  category2 = Category.find(category).children.sample.id
+  category3 = Category.find(category2).grandchildren.sample.id  
   @test_product=Product.create(name:"test",description:'test-description',size_id:1,brand:'brand',status_id:1,shipping_fee_id:1,shipping_date_id:1,shipping_method_id:1,purchase_status_id:1,price:1000,category_grandparent_id:i,category_parent_id:j,category_id:k,user_id:1,prefecture_id:1)
   @test_product.images.attach(io: File.open('app/assets/images/custom-banner-icon.png'), filename: 'custom-banner-icon.png',content_type: "image/jpg")
   num += 1
