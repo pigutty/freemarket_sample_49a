6f10afaddee0b6b803acc1259e7b298e3172d34b
xkatsuyax <ohagi.p@gmail.com>
2019-08-25 14:46:55 +0900

    fix　カラム統合とomniauthを修正

--- db/migrate/20190727094909_add_detail_to_users.rb
+++ db/migrate/20190727094909_add_detail_to_users.rb
@@ -9,5 +9,7 @@ class AddDetailToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :birth_month, :integer
     add_column :users, :birth_day, :integer
     add_column :users, :profile, :text
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_070844) do
+ActiveRecord::Schema.define(version: 2019_08_13_055807) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
