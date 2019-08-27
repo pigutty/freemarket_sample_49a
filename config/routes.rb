e4302bff8a0b6cee7dddc0d1fb2da81340cf3b12
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-27 19:51:45 +0900

    root_path変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'top#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :top, only: :index, defaults: { format: 'json' }
   end
