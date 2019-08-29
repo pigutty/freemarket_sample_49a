6126cc6ddb067a06651959e52f6929ca95f540da
pigutty <50047826+pigutty@users.noreply.github.com>
2019-08-29 11:38:56 +0900

    価格のセレクトボックス機能

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,8 +1,45 @@
 $(document).on('turbolinks:load', function(){
+  // 検索の並び替え
   $(document).on('change', '.order-select', function () {
     $('#search_submit').trigger('click');
   });
 
+  // 価格のセレクトボックスで価格が入力される
+  $(document).on('change', '#price_select', function () {
+    var value = $(this).val();
+    console.log(value);
+    if(value == 1){
+      $('#q_price_gteq').val(300);
+      $('#q_price_lteq').val(1000);
+    }
+    else if(value == 2){
+      $('#q_price_gteq').val(1000);
+      $('#q_price_lteq').val(5000);
+    }
+    else if(value == 3){    
+      $('#q_price_gteq').val(5000);
+      $('#q_price_lteq').val(10000);
+    }
+    else if(value == 4){    
+      $('#q_price_gteq').val(10000);
+      $('#q_price_lteq').val(30000);
+    }
+    else if(value == 5){
+      $('#q_price_gteq').val(30000);
+      $('#q_price_lteq').val(50000);
+    }
+    else if(value == 6){
+      $('#q_price_gteq').val(50000);
+      $('#q_price_lteq').val("");
+    }
+    else {
+      $('#q_price_gteq').val("");
+      $('#q_price_lteq').val("");
+    }
+  });
+
+
+  // クリアボタンを押した時にフォームをリセット
   $('#search-clear').click(function(e){
     $('.search-box').find('select').val("");
     $('.search-box').find('input').val("");
@@ -12,4 +49,7 @@ $(document).on('turbolinks:load', function(){
     e.preventDefault();
     return false;
   });
+
+
+
 });
