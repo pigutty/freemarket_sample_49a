c89af521f131d9269d4e6cc2f9b3349eb8c6c78a
tantan1984 <tantan198409@gmail.com>
2019-08-31 17:02:09 +0900

    ログイン時以外はリダイレクト
    ログインしていない時は商品一覧ページ・商品詳細ページ以外にアクセスしようとするとログインページになるようにしました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   protect_from_forgery with: :exception
   # before_action :registration_check  
   before_action :registration_view_params
