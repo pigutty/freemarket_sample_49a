f8db62a04ba3638c6a697c6f4815ca76480bbdec
xkatsuyax <ohagi.p@gmail.com>
2019-09-01 18:40:50 +0900

    パンくず、カテゴリーレベル２、３実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,7 +7,29 @@ crumb :mypage do
   parent :root
 end
 
-crumb :top do
+crumb :top1 do
+  if params[:id].to_i > 159
+    link Category.find(params[:id]).parent.grandparent.name, top_path(Category.find(params[:id]).parent.grandparent.id)
+  elsif params[:id].to_i > 13
+    link Category.find(params[:id]).grandparent.name, top_path(Category.find(params[:id]).grandparent.id)
+  else
+    link Category.find(params[:id]).name, top_path(params[:id])
+  end
+    parent :root
+end
+
+crumb :top2 do
+  if  params[:id].to_i > 159
+    link Category.find(params[:id]).parent.name, top_path(Category.find(params[:id]).parent.id)
+  elsif params[:id].to_i > 13
+    link Category.find(params[:id]).name, top_path(params[:id])
+  end
+  parent :top1
+end
+
+crumb :top3 do
+  if  params[:id].to_i > 159
   link Category.find(params[:id]).name, top_path(params[:id])
-  parent :root
+  end
+  parent :top2
 end
