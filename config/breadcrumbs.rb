2bfb7e9b16bf6752c0b8ed32ef9f7d7b5f2be875
xkatsuyax <ohagi.p@gmail.com>
2019-09-02 19:55:23 +0900

    パンくずリストユーザー完了

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,6 +7,31 @@ crumb :mypage do
   parent :root
 end
 
+crumb :profile do
+  link "プロフィール", new_user_profile_path
+  parent :mypage
+end
+
+crumb :mypage_card do
+  link "支払い方法", mypage_card_index_path
+  parent :mypage
+end
+
+crumb :credit_card_information do
+  link "クレジットカード情報入力", new_mypage_card_path
+  parent :mypage
+end
+
+crumb :person_info_registration do
+  link "本人情報の登録", new_user_identification_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", edit_user_profile_path
+  parent :mypage
+end
+
 crumb :top1 do
   if params[:id].to_i > 159
     link Category.find(params[:id]).parent.grandparent.name, top_path(Category.find(params[:id]).parent.grandparent.id)
